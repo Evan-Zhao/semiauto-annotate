37f5ee407ef572a42dc9730c2278a87a7380ef56
Yifan Zhao <evanzhao@umich.edu>
2019-07-15 13:26:28 +0800

    Limit yolo to known labels

--- labelme/backend/model_loader.py
+++ labelme/backend/model_loader.py
@@ -4,6 +4,12 @@ from .pose_estm_parser import PoseEstmParser
 
 class ModelLoader(object):
     def __init__(self):
-        self.yolo = YoloParser('yolo3/output.json')
+        from labelme.utils import Config
+
+        labels = Config.get('labels').keys()
+        self.yolo = YoloParser(
+            'yolo3/output.json',
+            accepted_label=labels
+        )
         self.pose_estm = PoseEstmParser('pose-estm/output.json')
         self.data = self.yolo.data + self.pose_estm.data
--- labelme/backend/yolo_parser.py
+++ labelme/backend/yolo_parser.py
@@ -4,9 +4,9 @@ from labelme.shape import Shape, LabeledPoint
 
 
 class YoloParser(object):
-    def __init__(self, filename, label_accepted=None):
+    def __init__(self, filename, accepted_label=None):
         self.data = None
-        self.label_accepted = label_accepted
+        self.accepted_label = accepted_label
         if filename is not None:
             self.load(filename)
         self.filename = filename
@@ -17,9 +17,13 @@ class YoloParser(object):
         with open(filename, 'r') as f:
             loaded = json.load(f, object_hook=lambda d: namedtuple('X', d.keys(), rename=True)(*d.values()))
         self.data = [self.yolo_dict_to_shape(yolo_dict) for yolo_dict in loaded]
+        self.data = [x for x in self.data if x is not None]
 
-    @staticmethod
-    def yolo_dict_to_shape(val):
+    def yolo_dict_to_shape(self, val):
+        label = val._1
+        if label not in self.accepted_label:
+            print(f'Dropped shape of label {label}')
+            return None
         loc = val.location
         points = [
             QPointF(loc.left, loc.top),
@@ -30,6 +34,6 @@ class YoloParser(object):
         points = [LabeledPoint(p) for p in points]
         return Shape.from_points(
             points,
-            label=val._1,
+            label=label,
             shape_type='rectangle'
         )
